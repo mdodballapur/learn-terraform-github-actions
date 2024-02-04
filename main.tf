8bd8cbb534cba36e3379a5f021dc75496fed83ac
Manohar Dodballapur <manohar.dodballapur@workday.com>
2024-02-03 21:51:30 -0800

    update main.tf by pointing backend to correct tfc org and workspace.

--- main.tf
+++ main.tf
@@ -15,10 +15,10 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "md-Terraform-Demos"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "Demo-github-actions"
     }
   }
 }
